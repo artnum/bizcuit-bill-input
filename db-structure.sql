ab70b5463bd45608e4cfa551c2fb3cd5bd8ca280
Etienne Bagnoud <etienne@artisan-numerique.ch>
2023-11-03 14:32:20 +0100

    DB Structure

--- db-structure.sql
+++ db-structure.sql
@@ -17,8 +17,11 @@ CREATE TABLE `facture` (
   `facture_conditions` varchar(30) DEFAULT '',
   `facture_qraddress` INT(10) UNSIGNED NULL,
   `facture_state` enum('INCOMING','OPEN','SENT','PAID','DELETED') NOT NULL DEFAULT 'INCOMING',
+  `facture_remainder` TINYINT UNSIGNED DEFAULT 0,
+  `facture_number` VARCHAR(140) DEFAULT '',
   PRIMARY KEY (`facture_id`)
 ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE INDEX `idxFactureState` ON `facture`(`facture_state`);
 
 CREATE TABLE `qraddress` (
     `qraddress_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
